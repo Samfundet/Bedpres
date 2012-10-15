3dfe7b000d23aa5027908978d021d69ba5323a7b
Jonas Amundsen <jonasba@gmail.com>
2012-10-16 01:33:16 +0200

    Commented out test data.

--- presentations.php
+++ presentations.php
@@ -5,7 +5,7 @@
       int mktime ([ int $hour = date("H") [, int $minute = date("i") [, int $second = date("s") [, int $month = date("n") [, int $day = date("j") [, int $year = date("Y") [, int $is_dst = -1 ]]]]]]] )
     */
 
-    new Presentation (
+    /*new Presentation (
         "Statoil",
         mktime ( 20, 00, 00, 10, 14, 2012 ),
         "Statoil har 40 årsjubileum, og dette feires med brask og bram på Studentersamfundet i Trondheim. For å avslutte det hele får vi besøk av Syntpopelektronika-kollektivet fra Norrköping Slagsmålsklubben som kommer til å få ALLE til å danse!",
@@ -27,7 +27,7 @@
         "Statoil",
         mktime ( 20, 00, 00, 10, 22, 2012 ),
         "Statoil har 40 årsjubileum, og dette feires med brask og bram på Studentersamfundet i Trondheim. For å avslutte det hele får vi besøk av Syntpopelektronika-kollektivet fra Norrköping Slagsmålsklubben som kommer til å få ALLE til å danse!",
-        20 );
+        20 );*/
 
     $upcoming_presentations = Presentation :: getUpcomingPresentations ();
 
