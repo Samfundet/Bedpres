73660f5f05d21c956fe36fd2ce061f6cb1e3efa3
Jonas Amundsen <jonasba@gmail.com>
2012-10-15 20:15:35 +0200

    The choice of presentation is now validated against all upcoming presentations.

--- register.php
+++ register.php
@@ -1,9 +1,20 @@
 <?php
+
+    require_once ( "presentations.php" );
+
+    $valid_presentations = array_map ( function ( $presentation ) {
+        return $presentation["name"] . ", " . date ( "d.m.y", $presentation["date"] );
+    }, $upcoming_presentations );
+
     if ( !empty ( $_POST["name"] ) &&
          !empty ( $_POST["email"] ) &&
          !empty ( $_POST["phone"] ) &&
          !empty ( $_POST["presentation"] ) ) {
-        $status = 0;
+        if ( in_array ( $_POST["presentation"], $valid_presentations ) ) {
+            $status = 0;
+        } else {
+            $status = 2;
+        }
     } else {
         $status = 1;
     }
