09019f8847e9febc5dfe8b2008fd038c48cafec3
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2012-10-18 23:11:24 +0200

    empty rails project for Bedpres system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bedpres::Application.config.secret_token = '5fd956b6523955974b59e802a183650c3b1a090337a4695e5a60be3e2d98f4b502fb0cd739d5157b0d44eccfe5679b2b9fcda3e4500e5b869d39661b12065b61'
