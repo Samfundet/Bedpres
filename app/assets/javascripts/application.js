82057056d13a8fa9bc7bfc3be24fc9ea98af4ef8
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2012-10-29 23:38:56 +0100

    updated pipeline files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require jquery.ui.all
+//= require jquery.ui.datepicker
+//= require jquery-ui-timepicker-addon
+//= require timepicker.js
