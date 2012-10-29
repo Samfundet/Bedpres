d247efbd562a124746cf3e76edc23b4587ce05ce
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2012-10-29 22:32:08 +0100

    added users controller and model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #flash[:success] = "Welcome, #{@user.name}.".html_safe
+      #session[:user_id] = @user.id
+      redirect_to root_path #@user
+    else
+      #flash.now[:failure] = "<span class='black'>Invalid</span> user data.".html_safe
+      render :new
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  firstname       :string(255)
+#  surname         :string(255)
+#  hashed_password :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+
+  attr_accessor :password, :password_confirmation
+  attr_accessible :firstname, :surname, :email, :password, :password_confirmation
+
+  validates_presence_of :firstname, :surname, :email
+
+  validates :email, :email_format => {:message => 'Invalid email'}, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :confirmation => true, :length => {:minimum => 6}
+
+  validates_presence_of :password_confirmation
+
+  before_save do |user|
+    hash_new_password
+    email.downcase!
+  end
+
+  def hash_new_password
+    self.hashed_password = BCrypt::Password.create(password)
+  end
+
+  class << self
+    def authenticate(email, password)
+      user = find_by_email(email.downcase)
+      return user if user &&
+        BCrypt::Password.new(user.hashed_password) == password
+      end
+    end
+
+  private
+    def lowercase_email
+      email.downcase!
+    end
+
+end
