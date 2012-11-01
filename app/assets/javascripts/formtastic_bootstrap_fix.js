5f3804d23c0698efecea67d432888fe49fc92bbf
Jonas Amundsen <jonasba@gmail.com>
2012-11-01 05:34:57 +0100

    Tuned the fix to support inline hints.

--- app/assets/javascripts/formtastic_bootstrap_fix.js
+++ app/assets/javascripts/formtastic_bootstrap_fix.js
@@ -1,8 +1,14 @@
 // See https://github.com/mjbellantoni/formtastic-bootstrap/issues/42.
 
 $(function () {
-    $('.formtastic .help-inline').each(function () {
+    $('.form-horizontal .help-inline').each(function () {
         var l = $(this);
         l.appendTo(l.siblings('.controls'));
+    });
+
+    $('.form-horizontal .help-block').each(function () {
+        var l = $(this);
+        l.removeClass("help-block");
+        l.addClass("help-inline");
     });
 });
