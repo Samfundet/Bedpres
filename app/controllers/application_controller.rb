2a1f0772bafdbe0f6132a241858a2cdfe59cd263
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2012-11-01 21:47:23 +0100

    implemented role-based authorization for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,32 @@
-class ApplicationController < ActionController::Base
-  before_filter :set_current_user
-  protect_from_forgery
+# encoding: UTF-8
 
-  def set_current_user
-    unless session[:member_id].nil?
-      @current_user = Member.find session[:member_id]
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_filter :set_current_user_for_model_layer_access_control
+
+  def current_user
+    begin
+      if session[:member_id]
+        @current_user ||= Member.find(session[:member_id])
+      #elsif session[:applicant_id] to be implemented with normal user login
+      #  @current_user ||= Applicant.find(session[:applicant_id])
+      end
+    rescue
+      @current_user = nil
     end
   end
+
+  def permission_denied 
+    flash[:failure] = "Du må logge inn for å gjøre dette"
+    #redirect_to :back
+    #rescue ActionController::RedirectBackError
+    redirect_to login_path
+  end
+
+  protected
+
+    def set_current_user_for_model_layer_access_control
+      Authorization.current_user = current_user
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  filter_access_to :all
 
   def new
     @user = User.new
