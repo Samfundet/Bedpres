3a4396720c02f46e357ed6a45786863f6704b2c0
Jonas Amundsen <jonasba@gmail.com>
2012-11-01 05:27:42 +0100

    Changed the way locales are included.

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Bedpres
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '{models}', '**', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :no
 
     # Configure the default encoding used in templates for Ruby 1.9.
