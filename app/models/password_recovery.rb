8021a59dcb49a90c587bf9581be46ed1b746a8c9
Dagrun <dagrunh@gmail.com>
2012-11-05 20:24:45 +0100

    annotate

--- app/models/password_recovery.rb
+++ app/models/password_recovery.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: password_recoveries
+#
+#  id            :integer          not null, primary key
+#  recovery_hash :string(255)
+#  user_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class PasswordRecovery < ActiveRecord::Base
   attr_accessible :user, :recovery_hash
 
