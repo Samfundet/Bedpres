3e495512c055ffc4b2fa57bee03901bba73a3e7a
Jonas Amundsen <jonasba@gmail.com>
2012-11-11 11:40:56 +0100

    Fixed a bug in checking of available slots.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -22,7 +22,7 @@ class ParticipationsController < ApplicationController
     if @presentation.canceled
       flash[:failure] = "Denne presentasjonen er desverre avlyst."
       redirect_to @presentation
-    elsif @presentation.users.size < @presentation.guest_limit
+    elsif @presentation.users.size == @presentation.guest_limit
       flash[:failure] = "Denne presentasjonen er desverre full."
       redirect_to @presentation
     elsif Time.now >= @presentation.presentation_date
