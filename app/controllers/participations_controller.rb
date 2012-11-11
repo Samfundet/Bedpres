9b1044e3a95617b1681c9c91ccf3fbdb34b75202
Jonas Amundsen <jonasba@gmail.com>
2012-11-11 11:40:33 +0100

    One cannot participate or cancel a participation on a canceled presentation.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -5,15 +5,24 @@ class ParticipationsController < ApplicationController
 
   def destroy
     @participation = Participation.where(:presentation_id => params[:presentation_id], :user_id => @current_user).first
-    @participation.destroy
-    flash[:success] = "Du er nå avmeldt presentasjonen."
-    redirect_to @participation.presentation
+
+    if @participation.presentation.canceled
+      flash[:failure] = "Denne presentasjonen er desverre avlyst."
+      redirect_to @participation.presentation
+    else
+      @participation.destroy
+      flash[:success] = "Du er nå avmeldt presentasjonen."
+      redirect_to @participation.presentation
+    end
   end
 
   def create
     @presentation = Presentation.find(params[:presentation_id])
 
-    if @presentation.users.size < @presentation.guest_limit
+    if @presentation.canceled
+      flash[:failure] = "Denne presentasjonen er desverre avlyst."
+      redirect_to @presentation
+    elsif @presentation.users.size < @presentation.guest_limit
       flash[:failure] = "Denne presentasjonen er desverre full."
       redirect_to @presentation
     elsif Time.now >= @presentation.presentation_date
