60bf8ec9a8dccce826c8f83f8ed03300137a5e96
Jonas Amundsen <jonasba@gmail.com>
2012-11-12 19:11:07 +0100

    One can now unparticipate from a canceled presentation.

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -6,14 +6,9 @@ class ParticipationsController < ApplicationController
   def destroy
     @participation = Participation.where(:presentation_id => params[:presentation_id], :user_id => @current_user).first
 
-    if @participation.presentation.canceled
-      flash[:error] = "Denne presentasjonen er desverre avlyst."
-      redirect_to @participation.presentation
-    else
-      @participation.destroy
-      flash[:success] = "Du er nå avmeldt presentasjonen."
-      redirect_to @participation.presentation
-    end
+    @participation.destroy
+    flash[:success] = "Du er nå avmeldt presentasjonen."
+    redirect_to @participation.presentation
   end
 
   def create
