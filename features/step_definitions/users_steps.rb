559be63b0106f7929e30317db8a96e311e3626f9
Jonas Amundsen <jonasba@gmail.com>
2012-11-23 20:24:39 +0100

    Creation of user only happens if the user does not exist. Password is updated accordingly.

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -1,9 +1,22 @@
 Given /^there is a user with email "(.*?)" and password "(.*?)"$/ do |email, password|
-  User.create!(
-      :firstname => "John",
-      :surname => "Doe",
-      :email => email,
-      :password => password,
-      :password_confirmation => password,
-  )
+  user = User.authenticate email, password
+
+  unless user
+    user = User.find_by_email email
+
+    if user
+      user.update_attributes!(
+          :password => password,
+          :password_confirmation => password,
+      )
+    else
+      User.create!(
+          :firstname => "John",
+          :surname => "Doe",
+          :email => email,
+          :password => password,
+          :password_confirmation => password,
+      )
+    end
+  end
 end
