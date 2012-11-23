5301fdaddef53477aab897ba1296a42e2aca9df2
Jonas Amundsen <jonasba@gmail.com>
2012-11-23 20:36:22 +0100

    Creation of member only happens if the member does not exist. Password is updated accordingly.

--- features/step_definitions/members_steps.rb
+++ features/step_definitions/members_steps.rb
@@ -1,9 +1,21 @@
 Given /^there is a member with email "(.*?)" and password "(.*?)"$/ do |email, password|
-  Member.create!(
-      :fornavn => "John",
-      :etternavn => "Doe",
-      :mail => email,
-      :passord => password,
-      :telefon => "22222222",
-  )
+  member = Member.authenticate email, password
+
+  unless member
+    member = Member.find_by_mail email
+
+    if member
+      member.update_attributes!(
+          :passord => password,
+      )
+    else
+      Member.create!(
+          :fornavn => "John",
+          :etternavn => "Doe",
+          :mail => email,
+          :passord => password,
+          :telefon => "22222222",
+      )
+    end
+  end
 end
