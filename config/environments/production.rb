48bb5b5ac78452e36a58e6065eeeadd50d944f1d
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-04 22:49:08 +0100

    really fixed mail this time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,13 +53,6 @@ Bedpres::Application.configure do
 
   config.action_mailer.default_url_options = { :host => "bedpres.samfundet.no" }
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.sendmail_settings = {
-    :location => '/usr/sbin/sendmail',
-    :arguments => '-i -t'
-  }
-
   # Enable threaded mode
   # config.threadsafe!
 
