89632416c260fe5309edac97b1ee9aed30cbc6ff
Jonas Amundsen <jonasba@gmail.com>
2013-03-07 21:09:32 +0100

    Step definitions for attendance supports members.

--- features/step_definitions/participations_steps.rb
+++ features/step_definitions/participations_steps.rb
@@ -1,20 +1,25 @@
-Given /^a user named "(.*?)" is on the attendance list for "(.*?)"$/ do |user_full_name, presentation_name|
+Given /^a (user|member) named "(.*?)" is on the attendance list for "(.*?)"$/ do |participle_type, user_full_name, presentation_name|
   begin
     step %(a user named "#{user_full_name}" should be on the attendance list for "#{presentation_name}")
   rescue
     # User was not on the attendance list. Put them there.
 
     presentation = Presentation.find_by_name(presentation_name)
-    user = User.find_by_firstname_and_surname(user_full_name.split.first, user_full_name.split.last)
+
+    if participle_type == "user"
+      participle = User.find_by_firstname_and_surname(user_full_name.split.first, user_full_name.split.last)
+    else
+      participle = Member.find_by_fornavn_and_etternavn(user_full_name.split.first, user_full_name.split.last)
+    end
 
     Participation.new(
-        :participle => user,
+        :participle => participle,
         :presentation => presentation,
     ).save(:validate => false).inspect
   end
 end
 
-Then /^a user named "(.*?)" should be on the attendance list for "(.*?)"$/ do |user_full_name, presentation_name|
+Then /^a (?:user|member) named "(.*?)" should be on the attendance list for "(.*?)"$/ do |user_full_name, presentation_name|
   user = Presentation.find_by_name(presentation_name).participants.find do |user|
     user.full_name == user_full_name
   end
@@ -22,7 +27,7 @@ Then /^a user named "(.*?)" should be on the attendance list for "(.*?)"$/ do |u
   user.should_not be_nil
 end
 
-Then /^a user named "(.*?)" should not be on the attendance list for "(.*?)"$/ do |user_full_name, presentation_name|
+Then /^a (?:user|member) named "(.*?)" should not be on the attendance list for "(.*?)"$/ do |user_full_name, presentation_name|
   expect { step %(a user named "#{user_full_name}" should be on the attendance list for "#{presentation_name}") }.to raise_error
 end
 
