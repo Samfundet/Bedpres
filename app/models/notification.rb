f72ccd31d8989bf664aa9b73fc46c3b25237473b
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-08 04:26:09 +0100

    removed reference to user_id and annotatet model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,11 +3,12 @@
 # Table name: notifications
 #
 #  id              :integer          not null, primary key
-#  user_id         :integer
 #  descritpion     :text
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  presentation_id :integer
+#  notifiable_type :string(255)
+#  notifiable_id   :integer
 #
 
 class Notification < ActiveRecord::Base
@@ -15,5 +16,5 @@ class Notification < ActiveRecord::Base
   belongs_to :notifiable, :polymorphic => true
   belongs_to :presentation
 
-  validates_presence_of :user_id, :user, :presentation_id, :presentation, :descritpion
+  validates_presence_of :presentation_id, :presentation, :descritpion
 end
